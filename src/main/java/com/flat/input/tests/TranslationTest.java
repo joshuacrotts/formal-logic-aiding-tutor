19f9012c0d493779c0314cab3c3c271530079adf
Joshua Crotts <joshuacrotts@yahoo.com>
2021-06-23 14:07:52 -0400

    Added translation API preliminaries.

---
+++ src/main/java/com/flat/input/tests/TranslationTest.java
@@ -0,0 +1,15 @@
+package com.flat.input.tests;
+
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
+
+public class TranslationTest {
+
+    public static void main(String[] args) {
+        System.setProperty("GOOGLE_API_KEY", "");
+        Translate translate = TranslateOptions.getDefaultInstance().getService();
+        Translation translation = translate.translate("Hola mundo");
+        System.out.printf("Translated text:\n\t%s\n", translation.getTranslatedText());
+    }
+}
