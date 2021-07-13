238d449bb6ef195a77744698627e8bd6abeed163
ccbrantley <c_brantl@uncg.edu>
2021-07-13 07:52:23 -0400

    Adding event for apply formula button.

--- src/main/java/com/flat/view/main/panes/center/children/algorithmselection/AlgorithmSelectionPane.java
+++ src/main/java/com/flat/view/main/panes/center/children/algorithmselection/AlgorithmSelectionPane.java
@@ -1,9 +1,13 @@
 package com.flat.view.main.panes.center.children.algorithmselection;
 
+import com.flat.models.json.algorithm.JsonAlgorithm;
 import com.flat.view.main.panes.center.children.algorithmselection.children.comboboxes.GeneralComboBox;
 import com.flat.view.main.panes.center.children.algorithmselection.children.comboboxes.PredicateComboBox;
 import com.flat.view.main.panes.center.children.algorithmselection.children.comboboxes.PropositionalComboBox;
+import java.util.Arrays;
+import java.util.List;
 import javafx.geometry.Pos;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.HBox;
 
 /**
@@ -14,6 +18,8 @@ public class AlgorithmSelectionPane extends HBox {
     private GeneralComboBox generalCombo = new GeneralComboBox();
     private PredicateComboBox predicateCombo = new PredicateComboBox();
     private PropositionalComboBox propositionalCombo = new PropositionalComboBox();
+    private List <ComboBox> comboBoxes = Arrays.asList(generalCombo, predicateCombo, propositionalCombo);
+    private JsonAlgorithm selectedAlgorithm = null;
 
     public AlgorithmSelectionPane () {
         this.initializeFx();
@@ -21,12 +27,81 @@ public class AlgorithmSelectionPane extends HBox {
 
     private void initializeFx () {
         this.setThisFx();
+        this.setGeneralComboFx();
+        this.setPredicateComboFx();
+        this.setPropositionalComboFx();
     }
 
     private void setThisFx () {
         super.getChildren().addAll(this.generalCombo, this.predicateCombo, this.propositionalCombo);
         super.setAlignment(Pos.CENTER);
         super.setSpacing(10);
+    }
+
+    private void setGeneralComboFx () {
+        this.setOnComboSelection(this.generalCombo);
+    }
+
+    private void setPredicateComboFx () {
+        this.setOnComboSelection(this.predicateCombo);
+    }
+
+    private void setPropositionalComboFx () {
+        this.setOnComboSelection(this.propositionalCombo);
+    }
+
+    private void setOnComboSelection (ComboBox _comboBox) {
+        _comboBox.valueProperty().addListener((obs, oldVal, newVal) -> {
+            if (newVal == null)
+                return;
+            this.selectedAlgorithm = (JsonAlgorithm)_comboBox.getValue();
+            this.comboBoxes.forEach(comboBox -> {
+                if (!_comboBox.equals(comboBox))
+                    comboBox.valueProperty().setValue(null);
+            });
+        });
+    }
+
+    // Getters for object's attributes.
+    public GeneralComboBox getGeneralCombo() {
+        return generalCombo;
+    }
+
+    public PredicateComboBox getPredicateCombo() {
+        return predicateCombo;
+    }
+
+    public PropositionalComboBox getPropositionalCombo() {
+        return propositionalCombo;
+    }
+
+    public List<ComboBox> getComboBoxes() {
+        return comboBoxes;
+    }
+
+    public JsonAlgorithm getSelectedAlgorithm() {
+        return selectedAlgorithm;
+    }
+
+    // Setters for object's attributes.
+    public void setGeneralCombo(GeneralComboBox generalCombo) {
+        this.generalCombo = generalCombo;
+    }
+
+    public void setPredicateCombo(PredicateComboBox predicateCombo) {
+        this.predicateCombo = predicateCombo;
+    }
+
+    public void setPropositionalCombo(PropositionalComboBox propositionalCombo) {
+        this.propositionalCombo = propositionalCombo;
+    }
+
+    public void setComboBoxes(List<ComboBox> comboBoxes) {
+        this.comboBoxes = comboBoxes;
+    }
+
+    public void setSelectedAlgorithm(JsonAlgorithm selectedAlgorithm) {
+        this.selectedAlgorithm = selectedAlgorithm;
     }
 
 }
---
+++ src/main/java/com/flat/view/main/panes/center/children/algorithmselection/listener/AlgorithmSelectionPaneListener.java
@@ -0,0 +1,28 @@
+package com.flat.view.main.panes.center.children.algorithmselection.listener;
+
+import com.flat.tools.eventbus.base.Event;
+import com.flat.tools.eventbus.base.Listener;
+import com.flat.view.main.panes.center.children.algorithmselection.AlgorithmSelectionPane;
+
+/**
+ *
+ * @author Christopher Brantley <c_brantl@uncg.edu>
+ */
+public class AlgorithmSelectionPaneListener implements Listener {
+    private AlgorithmSelectionPane algorithmSelectionPane;
+
+    public AlgorithmSelectionPaneListener (AlgorithmSelectionPane _algorithmSelectionPane) {
+        this.algorithmSelectionPane = _algorithmSelectionPane;
+    }
+
+    @Override
+    public void catchEvent(Event _event) {
+        switch (_event.getType()) {
+            case FORMULA_APPLY_BUTTON_PRESSED:
+                if (algorithmSelectionPane.getSelectedAlgorithm() != null)
+                    return;
+                break;
+        }
+    }
+
+}
